cf161414a35117cf5c11c10818cf236f7b561794
arapoport918 <arapoport918@gmail.com>
2024-09-02 13:51:30 -0400

    Created player detail component

--- baseball-players-angular/src/app/app.module.ts
+++ baseball-players-angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerDetailComponent
   ],
   imports: [
     BrowserModule,
